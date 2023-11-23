3b5cdc2278cbe6b34dc99303081cde06b280db08
aswin <aswin.cshajikumar@gmail.com>
2023-11-23 15:57:01 +0530

    js

--- javascript-base/javascript.js
+++ javascript-base/javascript.js
@@ -1,2 +1,73 @@
 console.log('hello javascript,from external javascript...')
 document.write("from javascript");
+
+
+var a;
+a=99;
+console.log("a:",a);
+
+
+//datatypes
+var str="this is a string";
+console.log("str:",str);
+console.log(typeof(str));
+
+var num=25;
+console.log("num:",num);
+console.log(typeof(num));
+
+var bool1=true;
+console.log("bool1:",bool1);
+console.log(typeof(bool1));
+
+var i = undefined;
+console.log("i:",i);
+console.log(typeof(i));
+
+var n = null;
+console.log("n:",n);
+console.log(typeof(n));
+
+//global object
+
+console.log(window);
+var bike ="hero honda";
+window.console.log("bike:",bike);
+
+//variable scope
+
+//var
+/*
+ -add to global object
+ -function scoped
+ -can be redeclared and modified at any time
+ */
+
+ {
+ var car="benz";
+ console.log("car:",car);
+ console.log("window.car:",window.car);
+ }
+ console.log("car from outside the block:",car);
+
+ //let
+ /*
+ -does not add to global object
+ -block scoped
+ -can't redeclare
+ -can update value later
+ -declaration and initialization
+*/
+
+{
+    let fruit = "apple";
+    console.log("fruit:",fruit);
+
+    fruit ="banana";
+    console.log("fruit:",fruit)
+}
+
+
+
+
+
