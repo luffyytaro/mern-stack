af7718a9dc4c18c7d2c4ea574b97a08deaed675c
aswin <aswin.cshajikumar@gmail.com>
2023-12-15 16:01:32 +0530

    ecommerce

--- e-commerce TASK/javascript.js
+++ e-commerce TASK/javascript.js
@@ -1,40 +1,12 @@
-//Synchronous Single Threaded Language
-
-//there are two types-synchronous and asynchronous
-//Javascript logs synchronous first then only asynchronous will be checked,if a asynchronous data came before a synchronous ,even though they came first the priority is synchronous.
-
-
-console.log("first line");
-console.log("second line");
-
-// setTimeout(()=> {
-//     console.log("from setTimeOut...");
-// },2000);
-
-let datas = fetch("")
-console.log("datas:",datas);//microtask queue has higher priority
-
-console.log("third line");
-
-
-//web space development
-//Jaascript engine - callback queue and microtask queue (higher priority)
-//Event Loop
-//Call Stack
-
-
-    let xhr = new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
     let btn = document.getElementById('btn');
     btn.addEventListener('click',()=> {
         xhr.send();
 
     });
- //GET 
- //POST
- //PUT
- //DELETE
 
- xhr.open("get",'https://jsonplaceholder.typicode.com/users');
+
+ xhr.open("get",'https://https://fakestoreapi.com/products.typicode.com/users');
 
 
  console.log("xhr:",xhr);
@@ -61,15 +33,14 @@ console.log("third line");
                 datas=datas+ `
                 <tr>
                 <td>${parsed_response[i].id}</td>
-                <td>${parsed_response[i].name}</td>
-                <td>${parsed_response[i].username}</td>
-                <td>${parsed_response[i].website}</td>
-                </tr>
+                <td>${parsed_response[i].title}</td>
+                <td>${parsed_response[i].price}</td>
+                <td>${parsed_response[i].description}</td>
+                <tr/>
                 `
             }
 
             content.innerHTML = datas;
-
 
         }else {
             console.log("failed");
