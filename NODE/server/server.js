79261e62bbc21e2f990cb1c72028ceb772577094
aswin <aswin.cshajikumar@gmail.com>
2023-12-22 15:54:53 +0530

    node

---
+++ NODE/server/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const name = "luffy";
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`hello ${name} Server running at http://${hostname}:${port}/`);
+});
