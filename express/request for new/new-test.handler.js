c6c1ee64bd2d10b89d21c5e0d2d635a6f4ba80ad
aswin <aswin.cshajikumar@gmail.com>
2024-02-27 13:25:57 +0530

    today's work

--- express/request for new/new-test.handler.js
+++ express/request for new/new-test.handler.js
@@ -1,4 +1,21 @@
-export function getData(req,res) {
-    console.log(req.query);
-    return res.end("data recieved");
+
+import fs from "fs";
+
+export function signup(req,res){
+    let {username , password} = req.body;
+    if(!username || !password) {
+        return res.status(400).json("username and password cannot be empty");
+    }
+    fs.readFile("./logins.json","utf-8",(error,data) =>{
+        let logins = data? JSON.parse(data) :[];
+        logins.push({username , password});
+        fs.writeFile("./logins.json", JSON.stringify(logins),error =>{
+            if(error) return res.status(500).json("error reciecved");
+            return res.status(200).json("succesfully recieved");
+        })
+    })
+}
+ 
+export function signin(req, res){
+    res.status(200).json("you're logged IN")
 }
--- express/router.js
+++ express/router.js
@@ -3,89 +3,60 @@ import fs from "fs";
 
 import { getData,postData } from "./request-handlers/test.handler.js";
 
+import {signup,signin} from "./request for new/new-test.handler.js"
+
 const router = Router();
 
 
-router.route("/").get((req,res) =>{
-    let x = Math.floor(Math.random()*100);
-    fs.readFile("./numbers.json","utf-8", (error,data) =>{
-        let numbers = data ?JSON.parse(data) :[];
-        numbers.push(x);
-        fs.writeFile("./numbers.json",JSON.stringify(numbers),() =>{
-            res.json(x);
-        })
-    })
-});
+router.route("/signup").post(signup);
+router.route("/signin").post(mw,signin);
 
-
-router.route("/1").get((req,res)=>{
-    let username = 
-    fs.readFile("./username.json","utf-8",(error,data) =>{
-        let username = data?JSON.parse(data) : [];
-        username.push();
-        fs.writeFile("./username.json",JSON.stringify(username),()=>{
-            res.json();
-        })
-    })
-});
-
-router.route("/4").get((req,res)=>{
-    let password = 
-    fs.readFile("./username.json","utf-8",(error,data) =>{
-        let password = data?JSON.parse(data) : [];
-        password.push();
-        fs.writeFile("./username.json",JSON.stringify(password),()=>{
-            res.json();
-        })
-    })
-});
+// router.route("/").get((req,res) =>{
+//     let x = Math.floor(Math.random()*100);
+//     fs.readFile("./numbers.json","utf-8", (error,data) =>{
+//         let numbers = data ?JSON.parse(data) :[];
+//         numbers.push(x);
+//         fs.writeFile("./numbers.json",JSON.stringify(numbers),() =>{
+//             res.json(x);
+//         })
+//     })
+// });
 
 
 
-router.route("/3").get(middle,(req,res) => res.send("authorized"));
-router.route("/5").get(middle,(req,res) => res.send("authorized"));
 
-router.route("/2").get(mw,(req,res) => res.send("authorized"));
+// router.route("/2").get(mw,(req,res) => res.send("authorized"));
 router.route("/get-data").get(getData);
 router.route("/post-data").post(postData);
 
 export default router;
 
-
-
-function middle(req, res, next){
-    let {id} = req.query;
-    fs.readFile("./username.json","utf-8", (error,data) =>{
-        let username = data? JSON.parse(data) :[];
-        if (username.includes(String(id))) {
-            return next();
-        }
-        res.send("unauthorized access")
-    })
-}
-
-function middleware(req, res, next){
-    let {id} = req.query;
-    fs.readFile("./username.json","utf-8", (error,data) =>{
-        let password = data? JSON.parse(data) :[];
-        if (password.includes(String(id))) {
-            return next();
-        }
-        res.send("unauthorized access")
-    })
-}
-
-
-function mw(req, res, next){
-    let {id} = req.query;
-    fs.readFile("./numbers.json","utf-8", (error,data) =>{
+function mw(req,res,next) {
+    let {username, password} = req.body;
+    fs.readFile("./logins.json","utf-8",  (error,data)=>{
         let numbers = data? JSON.parse(data) :[];
-        if (numbers.includes(Number(id))) {
+        let user = numbers.find(val=>val.username===username);
+        if(user && user.password===password) {
             return next();
         }
-        res.send("unauthorized access")
+        res.status(401).send("unauthorized access");
     })
 }
+
+
+
+
+
+// function mw(req, res, next){
+//     let {id} = req.query;
+//     fs.readFile("./numbers.json","utf-8", (error,data) =>{
+//         let numbers = data? JSON.parse(data) :[];
+//         if (numbers.includes(Number(id))) {
+//             return next();
+//         }
+//         res.send("unauthorized access")
+//     })
+// }
 
 
 
